175f5571ea215078767af3b2a3ace2f1be0b2d54
Yescii <cgonzalez.jesse@gmail.com>
2021-09-29 01:27:35 -0400

    added a post route to create a new pokemon character from user input from the new page

--- server.js
+++ server.js
@@ -7,19 +7,45 @@ const pokedex = require("./models/pokemon.js");
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 
-app.get("/index", (req, res) => {
+app.use(express.urlencoded({ extended: false }));
+
+//INDEX
+app.get("/pokemon", (req, res) => {
   res.render("index.ejs", { pokedex: pokedex });
 });
 
-app.get("/index/new", (req, res) => {
+//NEW
+app.get("/pokemon/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.get("/index/:idx", (req, res) => {
+//SHOW
+app.get("/pokemon/:idx", (req, res) => {
   // get all types into one string
   let idxType = pokedex[req.params.idx].type.join(" ");
 
   res.render("show.ejs", { character: pokedex[req.params.idx], type: idxType });
+});
+
+// CREATE
+app.post("/pokemon", (req, res) => {
+  let arrTypes = [];
+  arrTypes = String(req.body.type).split(" ");
+  let obj = {
+    name: req.body.name,
+    img: req.body.img,
+    type: arrTypes,
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.atack,
+      defense: req.body.defense,
+      spattack: req.body.spattack,
+      spdefense: req.body.spdefense,
+      speed: req.body.speed,
+    },
+  };
+  pokedex.push(obj);
+  res.redirect("/pokemon");
 });
 
 app.listen(PORT, () => {});
