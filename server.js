964f558a2836ac6abab5ad07b2fde0eb916c76ca
Yescii <cgonzalez.jesse@gmail.com>
2021-09-28 21:52:14 -0400

    added a get route for show.ejs

--- server.js
+++ server.js
@@ -10,5 +10,10 @@ app.use("/css", express.static(__dirname + "public/css"));
 app.get("/index", (req, res) => {
   res.render("index.ejs", { pokedex: pokedex });
 });
+app.get("/index/:idx", (req, res) => {
+  let idxType = pokedex[req.params.idx].type.join(" ");
+
+  res.render("show.ejs", { character: pokedex[req.params.idx], type: idxType });
+});
 
 app.listen(PORT, () => {});
