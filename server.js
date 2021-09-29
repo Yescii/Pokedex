c333f3c97f9f449e3eb128e6dbaadff94f04217a
Yescii <cgonzalez.jesse@gmail.com>
2021-09-28 23:19:52 -0400

    add route for new page and organized code

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.use("/css", express.static(__dirname + "public/css"));
 app.get("/index", (req, res) => {
   res.render("index.ejs", { pokedex: pokedex });
 });
+
+app.get("/index/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 app.get("/index/:idx", (req, res) => {
+  // get all types into one string
   let idxType = pokedex[req.params.idx].type.join(" ");
 
   res.render("show.ejs", { character: pokedex[req.params.idx], type: idxType });
